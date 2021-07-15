e3bf4c746bd55b0ea524c0e5be300eb091546baa
Makar Kovynia <mak.kovynia@gmail.com>
2021-07-15 16:02:55 +0300

    forkio

---
+++ dist/js/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__list__2').classList.toggle('active');
+  });
